0986fc23b3d01276aa19472c45e54350b67e50a2
saviiosousa <saviiosousa@gmail.com>
2022-10-07 21:44:31 -0300

    master - commit 01

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+let nome = 'Digital College';
+console.log(nome, typeof nome)
+nome = 10;
+console.log(nome, typeof nome)
+nome = 3.141592658;
+console.log(nome, typeof nome)
+nome = true;
+console.log(nome, typeof nome)
+nome = 1 + 1;
+console.log(nome, typeof nome)
+nome = '1' + '1';
+console.log(nome, typeof nome)
+nome = false + 15;
+console.log(nome, typeof nome)
+nome = true + 15;
+console.log(nome, typeof nome)
+nome = 1 + '1';
+console.log(nome, typeof nome)
+
+
